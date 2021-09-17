99f9c0cedd7aafe77cfb21e51c644100ed4790c4
MONTCHO shegun <54720237+Shek863@users.noreply.github.com>
2021-09-17 13:34:22 +0100

    commit a revoir

--- ShareToWhatsapp.kt
+++ ShareToWhatsapp.kt
@@ -18,4 +18,31 @@ fun share (message:String) {
   ///let's go
   context.startActivity(intent)
   
+  
+//           // val uri = Uri.fromFile(viewModel.file.value)
+//            val uri : Uri = FileProvider.getUriForFile(
+//                requireContext(),
+//                requireContext().applicationContext.packageName.toString() + ".provider",
+//                viewModel.file.value!!
+//            )
+//
+//            val share = Intent()
+//            share.action = Intent.ACTION_SEND
+//            share.type = "application/pdf"
+//            //share.putExtra(Intent.EXTRA_TEXT, viewModel.getMessage())
+//            share.putExtra(Intent.EXTRA_TEXT, "Bonjour Mr Fabrice Dossou\n Voci votre reçu:")
+//            share.putExtra(Intent.EXTRA_STREAM, uri)
+//
+//            val pm: PackageManager = requireContext().getPackageManager()
+//            val matches = pm.queryIntentActivities(share, 0)
+//            var best: ResolveInfo? = null
+//            for (info in matches)
+//                if (info.activityInfo.packageName.startsWith("com.whatsapp") || info.activityInfo.name.toLowerCase().contains("whatsapp"))
+//                    best = info
+//            if (best != null) share.setClassName(
+//                best.activityInfo.packageName,
+//                best.activityInfo.name
+//            )
+//            requireContext().startActivity(share)
+  
 }
